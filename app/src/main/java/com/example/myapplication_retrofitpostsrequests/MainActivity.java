43020641c14e587dd8d5569cf3125b5ea75bf828
xkadmx <zuzanna.rosinska@yahoo.com>
2019-06-25 22:46:51 +0200

    moving List content to getPosts()

--- app/src/main/java/com/example/myapplication_retrofitpostsrequests/MainActivity.java
+++ app/src/main/java/com/example/myapplication_retrofitpostsrequests/MainActivity.java
@@ -33,6 +33,38 @@ public class MainActivity extends AppCompatActivity {
 
         jsonPlaceHolderAPI = retrofit.create(JsonPlaceHolderAPI.class);
 
+        getPosts();
 
+    }
+    public void getPosts(){
+        Call<List<Post>> call = jsonPlaceHolderAPI.getPosts();
+        call.enqueue(new Callback<List<Post>>() {
+            @Override
+            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
+                if(!response.isSuccessful()){
+                    textViewResult.setText("Code: " + response.code());
+                    return;
+                }
+
+
+                List  <Post> posts = response.body();
+                for(Post post : posts){
+                    String content ="";
+                    content += "ID: " + post.getId() + "\n";
+                    content += "User ID: " + post.getUserId() + "\n";
+                    content += "Title: " + post.getTitle() + "\n";
+                    content += "Text: " + post.getText() + "\n\n";
+
+                    textViewResult.append(content);
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<List<Post>> call, Throwable t) {
+                textViewResult.setText(t.getMessage());
+
+            }
+        });
     }
 }
